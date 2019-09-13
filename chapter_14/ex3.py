358f8ba7b839e0278437f7c1b6a5bbc3e1908643
Erik <erik.kristofer.anderson@gmail.com>
2019-09-13 12:55:40 -0500

    works for current directory, organized into functions

--- chapter_14/ex3.py
+++ chapter_14/ex3.py
@@ -7,41 +7,52 @@
 # first task... read the contents of a directory
 import os
 
-cmd = 'ls'
-fp = os.popen(cmd)
-res = fp.read()
-print(res)
-
-lines = res.split('\n') # TODO this breaks by splitting by spaces in
+def get_ls():
+    cmd = 'ls'
+    fp = os.popen(cmd)
+    res = fp.read()
+    lines = res.split('\n')
+    return lines # TODO this breaks by splitting by spaces in
 # file names. for now I will sanatize the filenames of spaces
 # but later I'd like to generalize this to handle spaces
 
 # next task: get the checksum for each of these
 # and make a dict of checksum to file names
 ### TODO generalize this to relaatave or absolute path
-checksum_dict = {}
-for line in lines:
+
+def make_checksum_dict():
+    lines = get_ls()
+    checksum_dict = {}
+    for line in lines:
 
 
-    if '.' in line:
+        if '.' in line:
 
-        filename = line
-        cmd = 'md5 ' + filename
-        fp = os.popen(cmd)
-        print(type(fp.read()))
-        #checksum = fp.read()
-        checksum = 'asdf'
-        print(type(checksum))
-        checksum = checksum[-32:-3]
-        #print(checksum, filename)
-        if checksum in checksum_dict:
-            checksum_dict[checksum].append(filename)
-        else:
-            checksum_dict[checksum] = [filename]
-    else: # if '.' not in line
-        pass # search the stuff in that directory
+            filename = line
+            cmd = 'md5 ' + filename
+            fp = os.popen(cmd)
+            checksum = fp.read()
 
+
+            checksum = checksum[-32:-3]
+
+            if checksum in checksum_dict:
+                checksum_dict[checksum].append(filename)
+            else:
+                checksum_dict[checksum] = [filename]
+        else: # if '.' not in line
+            pass # search the stuff in that directory
+
+    return checksum_dict
+
+
+
+
+
+checksum_dict = make_checksum_dict()
 #print(checksum_dict)
-
 for item in checksum_dict.items():
     print(item)
+
+
+# now: move code into functions
