4a81762ad63ccaa8240cfea03c8637a5864288d0
Erik <erik.kristofer.anderson@gmail.com>
2019-09-13 12:32:55 -0500

    second commit of ch14 ex3

--- chapter_14/ex3.py
+++ chapter_14/ex3.py
@@ -11,29 +11,29 @@ cmd = 'ls'
 fp = os.popen(cmd)
 res = fp.read()
 
-lines = res.split('\n')
+lines = res.split('\n') # TODO this breaks by splitting by spaces in
+# file names. for now I will sanatize the filenames of spaces
+# but later I'd like to generalize this to handle spaces
 
-# next task: get the checksum for each of these, and store to a list
-checksums = []
+# next task: get the checksum for each of these
+# and make a dict of checksum to file names
+### TODO generalize this to relaatave or absolute path
+checksum_dict = {}
 for line in lines:
 
     if '.' in line:
         filename = line
         cmd = 'md5 ' + filename
         fp = os.popen(cmd)
-        res = fp.read()
-        checksums.append(res)
+        checksum = fp.read()
+        checksum = checksum[-32:-3]
+        #print(checksum, filename)
+        if checksum in checksum_dict:
+            checksum_dict[checksum].append(filename)
+        else:
+            checksum_dict[checksum] = [filename]
 
+#print(checksum_dict)
 
-next task: identify duplicate checksums
-duplicates = []
-for checksum in checksums:
-    for checksum2 in checksums:
-        if checksum == checksum2:
-            duplicates.append(checksum)
-
-
-print(duplicates)
-# this doesn't work right because ... checksum and checksum2 come from the same list so there
-# will always be duplicats
-# commit this and backup and rework it
+for item in checksum_dict.items():
+    print(item)
